2e985598e0d2d04630c6f221166cc72aed047a84
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-13 09:48:27 +0530

    using Subscribe&Unsbuscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval} from 'rxjs'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy{
+  private firstObsSubscription!: Subscription
+  
 
+   ngOnInit() {
+    //  interval(periond: 1000).subscribe(next :count => {
+    //   console.log(count);
+    //  }) //**this is not work in angular16 */
+    
+  this.firstObsSubscription =  interval(1000).subscribe(count => {
+      console.log(count);
+    });
+   }
+   ngOnDestroy(): void {
+     this.firstObsSubscription.unsubscribe();
+   }
 }
