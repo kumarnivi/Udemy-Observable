b50f1da2aeb607950e10de1d067a3ecf17f82b49
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-13 20:45:31 +0530

    Binding a custom Observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval} from 'rxjs'
+import { Subscription, interval, Observable} from 'rxjs'
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,9 +14,23 @@ export class HomeComponent implements OnInit, OnDestroy{
     //   console.log(count);
     //  }) //**this is not work in angular16 */
     
-  this.firstObsSubscription =  interval(1000).subscribe(count => {
-      console.log(count);
-    });
+  // this.firstObsSubscription =  interval(1000).subscribe(count => {
+  //     console.log(count);
+  //   }); //**this is auto obsovable */
+
+  // ! custom Observable ! //
+  const customIntervalObservable = Observable.create((observer) => {
+    let count =0;
+    setInterval(handler: () => {
+      observer.next(count)
+       count++;
+    }, timeout: 1000);
+
+  });
+
+this.firstObsSubscription =  customIntervalObservable.subscribe(data => {
+    console.log(data)
+  });
    }
    ngOnDestroy(): void {
      this.firstObsSubscription.unsubscribe();
