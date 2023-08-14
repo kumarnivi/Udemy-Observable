3a9a915ed528a4502d53936d16f8dbcf05dd6008
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-14 08:46:29 +0530

    subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'obs_udemy';
+export class AppComponent implements OnInit {
+  private activatedSubscription!: Subscription;
+userActivated = false;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activatedSubscription = this.userService.activatedEmitter.subscribe(
+      didActivated => {
+        this.userActivated = didActivated;
+      }
+    );
+  }
 }
