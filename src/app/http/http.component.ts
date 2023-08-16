2b6e9b130bab25d16875825243110b2d4d13cdaa
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-16 13:16:29 +0530

    Using Rxjs Operators

--- src/app/http/http.component.ts
+++ src/app/http/http.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
 @Component({
   selector: 'app-http',
   templateUrl: './http.component.html',
@@ -10,7 +11,9 @@ export class HttpComponent {
 
   constructor(private http: HttpClient) {} //** inject the http client */
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.FetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -21,14 +24,35 @@ export class HttpComponent {
     )
     .subscribe(responseData => {
       console.log(responseData)
-    })
+    }) //** here can send request  */
   }
 
   onFetchPosts() {
     // Send Http request
+    this.FetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private FetchPosts() {
+     this.http.get('https://backend-setup-7f3ed-default-rtdb.firebaseio.com/posts.json')  //**no need 2n argument bcz only get the data */
+     .pipe(map(responseData => {
+      const postArray:any[] = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postArray.push({responseData, id:key})
+        }
+       
+      }
+      return postArray
+     }))                                                      //** get some data from map operator */
+     .subscribe(responseData => {
+      //...
+      console.log(responseData)
+    })
+    
+   
   }
 }
