7367c246a51d357146ae66db1df974620e204224
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-16 07:40:30 +0530

    add upperCase and Date pipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { PipesComponent } from './pipes/pipes.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'user/:id', component: UserComponent}
+  {path: 'user/:id', component: UserComponent},
+  {path: 'pipes', component: PipesComponent}
 ];
 
 @NgModule({
