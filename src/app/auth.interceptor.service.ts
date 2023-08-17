e2213fa5b09b2c66a683abf8048b3c7e6fce7a20
kumarnivi <nivethikashivakumar56@gmail.com>
2023-08-17 13:23:37 +0530

    Response Interceptor

--- src/app/auth.interceptor.service.ts
+++ src/app/auth.interceptor.service.ts
@@ -1,11 +1,22 @@
- import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+ import { HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs";
 
 
 export class AuthInteceptorService implements  HttpInterceptor {
  intercept(req: HttpRequest<any>, next: HttpHandler) {
-     
     console.log(' req is a awy');
-     return next.handle(req);
+    console.log(req.url)
+     const modifiedRequest = req.clone(
+        {headers: req.headers.append('Auth', 'hxy')
+    });
+     return next.handle(modifiedRequest).pipe(
+        tap(event => {
+        console.log(event)
+        if (event.type === HttpEventType.Response) {
+            console.log(' respons aeeaived , body data')
+            console.log(event.body)
+        }
+     }))
      
  }
 }
